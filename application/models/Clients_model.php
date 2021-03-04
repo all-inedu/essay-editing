423de19b7adfdf0ba449bb4d406e2152ff59f355
annur25 <hafidz.bdt@gmail.com>
2021-03-04 13:55:24 +0700

    Update 04/03/2021

--- application/models/Clients_model.php
+++ application/models/Clients_model.php
@@ -59,7 +59,7 @@ class Clients_model extends CI_model
         $this->db2->join('tbl_prog', 'tbl_prog.prog_id=tbl_stprog.prog_id');
         $this->db2->where('tbl_prog.main_number', 1);
         $this->db2->where('tbl_stmentor.mt_id1 !=', "");
-        $this->db2->where('tbl_stmentor.stmentor_id >', 0);
+        $this->db2->where('tbl_stmentor.stmentor_id >', 342);
         return $this->db2->get()->result_array();
     }
 
